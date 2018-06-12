3de1147261a77ef7eaa50ec843a2c22648251708
Suchitra Naik <susanaik@gmail.com>
2018-06-12 20:12:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,50 @@ class ApplicationController < Sinatra::Base
   get '/' do
 
   end
+
+  #CREATE Action
+
+    get '/posts/new' do
+      erb :new
+    end
+
+    post '/posts' do
+      puts params
+      @post = Post.new(name: params[:name],content: params[:content])
+      @post.save
+      erb :index
+    end
+
+    #READ
+
+    get '/posts' do
+      @posts = Post.all
+      erb :index
+    end
+
+    get "/posts/:id" do
+      @post = Post.find_by(params[:id])
+      erb :show
+    end
+
+    #Update
+
+      get "/posts/:id/edit" do
+        #binding.pry
+        @post = Post.find(params[:id])
+        erb :edit
+      end
+
+      patch "/posts/:id" do
+        @post = Post.update(name: params[:name], content: param[:content])
+        erb :show
+      end
+
+    # DELETE
+    delete "/posts/:id/delete" do
+      @post = Post.where(id: params[:id]).destroy_all
+      puts @post.name
+      erb :delete
+    end
+
 end
